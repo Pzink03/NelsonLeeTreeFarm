48304d6a1fa5c39903f9577f3b7ba57cb8604a1d
Pete <pete.zink03@gmail.com>
2024-06-26 10:40:02 -0400

    Fixed Banner and updated home buttons

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,5 @@
+import { useLocation } from "react-router-dom"
+
 type BannerProps = {
     title?: string,
     subtitle?: JSX.Element,
@@ -6,6 +8,9 @@ type BannerProps = {
 }
 
 export function Banner({title, subtitle, image, style}: BannerProps){
+
+    const location = useLocation()
+
     return (
     <div className={`w-full flex justify-center mt-14 relative shadow-2xl ${style}`}>
             <img
@@ -13,7 +18,7 @@ export function Banner({title, subtitle, image, style}: BannerProps){
             className={`h-full object-cover lg:object-center object-bottom w-full absolute ${style}`}
             />
             <div className={`absolute inset-0 bg-black opacity-40 h-full ${style}`}></div>
-            <div className="max-w-lg font-helvetica md:max-w-5xl h-96 gap-4 pt-[150px] md:pt-[600px] flex flex-col justify-center items-center z-40">
+            <div className={`max-w-lg font-helvetica md:max-w-5xl h-96 gap-4 pt-[150px] ${location.pathname === '/' ? 'md:pt-[600px]' : ''}  flex flex-col justify-center items-center z-40`}>
                 <h1 className="z-50 text-white font-black text-2xl md:text-5xl text-center">
                 {title}
                 </h1>
--- src/components/HomeButtons.tsx
+++ src/components/HomeButtons.tsx
@@ -6,11 +6,11 @@ export function HomeButtons(){
         <>
             <div className="flex">
                 <div className="border-b-2 border-r-2 px-10 py-4 mt-4 z-60">
-                    <Button className="px-10 py-6 bg-gray-400">Inventory</Button>
+                    <Button variant={"outline"} size={"lg"} className=" bg-transparent text-lg text-white">Learn More</Button>
                 </div>
                 <div className="border-b-2 px-10 py-4 mt-4 z-60">
                     <Link to={"/contact"}>
-                        <Button className="px-10 py-6 bg-gray-400">Contact Us</Button>
+                        <Button variant={"outline"} size={"lg"} className="bg-transparent text-lg text-white">Contact Us</Button>
                     </Link>
                 </div>
             </div>
