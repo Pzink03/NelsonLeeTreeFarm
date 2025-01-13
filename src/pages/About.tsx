b87cf28ff5a929b96e79827102946b65fa3b0200
Pete <pete.zink03@gmail.com>
2025-01-13 18:49:11 -0500

    Code Clean up. Created an FAQ component for easier readability, and created a reusable OurPromiseCards component to accept props for clearer code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,125 +1,8 @@
 import { Banner } from "@/components/Banner";
+import { Faq } from "@/components/Faq";
 import { NowHiring } from "@/components/NowHiring";
-// import { Header } from "@/components/Header";
-import { HandshakeIcon,  NotebookPenIcon,  TreePine } from "lucide-react";
-// import { FaLocationDot } from "react-icons/fa6";
-import { TbGardenCart, TbTruckDelivery } from "react-icons/tb";
-
 
 export function About(){
-
-    const frequentlyAskedQuestions = [
-        // {
-        //     id: 1,
-        //     title: 'what payment do you accept?qwegqwegqwegqweg',
-        //     response: 'asdgsadg assdfg dgasgasd gasfdg',
-        //     child: (
-        //         <>
-        //             <DollarSign size={70} color="green" />
-        //         </>
-        //     ),
-        // },
-        {
-            id: 2,
-            title: 'Do you offer delivery?',
-            response: (
-                <>
-                    <p>we hope to provide delivery in the near future. ( I hope Santa brings me a new box truck this year 😁).</p>
-                    <p>But seriously, special circumstances, larger orders could be considered for delivery, the delivery would be an additional cost and covered by the customer.</p>
-                </>
-            ),
-            child: (
-                <>
-                    <TbTruckDelivery size={70} color="orange" />
-                </>
-            ),
-        },
-        {
-            id: 3,
-            title: 'What types of trees and plants do you offer?',
-            response: 'We specialize in trees and plants that are well-suited for our local climate. Whether you’re looking for shade trees, ornamental plants, or native species, we have something for every garden or landscape.',
-            child: (
-                <>
-                    <TreePine size={70} color="green"/>
-                </>
-            ),
-        },
-        {
-            id: 4,
-            title: 'How do you ensure sustainability in your practices?',
-            response: 'We follow eco-friendly practices such as water conservation, using organic fertilizers, and promoting biodiversity. We’re committed to reducing our environmental footprint while providing top-quality products.',
-            child: (
-                <>
-                    <HandshakeIcon size={70} color="black" />
-                </>
-            ),
-        },
-
-
-        {
-            id: 8,
-            title: 'Can you help with landscaping advice?',
-            response: 'Sure, we can help in several ways: Plant or Tree selection based on the USDA Plant Hardiness Zone Map Kentucky falls into zones 6 or 7 depending on location. Additional information from customers helps us direct them to the proper plant: ',
-            subresponse: <ul className="list-disc px-6">
-                <li>
-                Type of plant or tree desired.
-                </li>
-                <li>
-                Location of where to plant on their property: full sun, partial sun/shade…etc!
-                </li>
-                <li>
-                Best time to plant, water, fertilize…etc!
-                </li>
-                <li>
-                Companion plants
-                </li>
-                <li>
-                For more critical issues, insect damage, diseases, plant stress…we recommend customers to
-consider contacting their respective local county extension offices for soil testing and insect or
-disease identification.
-                </li>
-                <li>
-                We always recommend soil testing prior to planting!
-                </li>
-            </ul>,
-            child: (
-                <>
-                    <TbGardenCart size={70} color="red" />
-                </>
-            ),
-        },
-        {
-            id: 9,
-            title: 'How can I place an order?',
-            response: 'You can place an order by contacting us directly at our farm, via phone, or email. We’re here to make the process as easy as possible.',
-            child: (
-                <>
-                    <NotebookPenIcon size={70} color="blue"/>
-                </>
-            ),
-        },
-        // {
-        //     id: 10,
-        //     title: 'qwegqwegqwegwe',
-        //     response: 'asd fasas FASD SADF ASDFG ',
-        //     child: (
-        //         <>
-        //             <TreePine size={70} color="green" />
-        //         </>
-        //     ),
-        // },
-        // {
-        //     id: 11,
-        //     title: 'qwregqwergqwerg',
-        //     response: 'dsfgsdfgsdfg dsfgsdfg sdfgsd fsdfg sdfg',
-        //     child: (
-        //         <>
-        //             <Clock size={70} color="blue"/>
-        //         </>
-        //     ),
-        // }
-    ]
-
     return (
         <>
             <Banner title={"Family Owned and Operated"} image="/assets/images/MainPic.png" subtitle="Nelson Lee Tree Farm is rooted in Kentucky, providing trees and plants that thrive in our climate. As a family-owned business, we’re passionate about nurturing the environment and helping our community grow. Our sustainable practices ensure that every tree, plant, and product we offer not only beautifies your space but also contributes to a greener, healthier world." />
@@ -138,12 +21,12 @@ disease identification.
                                     </header>
                                     <p className=" pt-10 font-raleway text-center  lg:text-left text-xl ">
                                     The owners/managers of Nelson Lee Tree Farm, LLC are local, originally from Lexington, Kentucky, eventually transplanting to Southern Woodford County where they now find themselves deeply rooted on a 30+ acre farm on the corner of Troy Pike (Hwy 33) and Cummins Ferry Road.
-<br></br>
-<br></br>
-What began as a dream, a vision, lots of prayer, moments of uncertainty and admittedly some doubt, the tree farm turned into a reality with lots of hard work and the blessings of God!
-<br></br>
-<br></br>
-The owner has worked in a few of the local garden centers over the years and on his previous mini-farm, planted and maintained various trees and shrubs while managing vegetable gardens with modest success. This tree farm is a new kind of business, but he has significant experience in other business ventures.
+                                    <br></br>
+                                    <br></br>
+                                    What began as a dream, a vision, lots of prayer, moments of uncertainty and admittedly some doubt, the tree farm turned into a reality with lots of hard work and the blessings of God!
+                                    <br></br>
+                                    <br></br>
+                                    The owner has worked in a few of the local garden centers over the years and on his previous mini-farm, planted and maintained various trees and shrubs while managing vegetable gardens with modest success. This tree farm is a new kind of business, but he has significant experience in other business ventures.
                                     </p>
                                 </div>
                             </div>
@@ -151,27 +34,9 @@ The owner has worked in a few of the local garden centers over the years and on 
                     </div>
                 </div>
                 <h1 className="my-11 pb-2 text-center border-b-4 border-primary-green md:text-3xl text-xl font-medium">Frequently Asked Questions</h1>
-                <ul className="grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] w-full md:max-w-6xl">
-                    {frequentlyAskedQuestions.map((question) => (
-                        <li key={question.id} className="py-10 md:p-10 gap-2 border-b-2 odd:border-r-2 flex flex-col justify-start items-center">
-                            <div className=" rounded-full p-4" >
-                                {question.child}
-                            </div>
-                            <h1 className="text-2xl font-playwrite text-center font-semibold">
-                                {question.title}
-                            </h1>
-                            <p className="text-lg text-gray-500 font-raleway ">
-                                {question.response}
-                            </p>
-                            <p className="text-lg text-gray-500 font-raleway ">
-                                {question.subresponse}
-                            </p>
-                        </li>
-                    ))}
-                </ul>
+                <Faq />
                 <NowHiring />
                 <p className="max-w-5xl font-helvetica text-center text-base py-10 md:px-20 px-4">The owners of Nelson Lee Tree Farm have been avid supporters of the Lexington Rescue Mission since 2002, therefore Nelson Lee Tree Farm will direct a percentage of their profits to the Rescue Mission and their respective ministries!</p>
-
             </div>
         </>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Banner } from "@/components/Banner";
 import { HomeButtons } from "@/components/HomeButtons";
+import { OurPromiseCards } from "@/components/OurPromiseCards";
 import { ServiceCard } from "@/components/ServiceCard";
 import { Handshake, Leaf, Sprout } from "lucide-react";
 
@@ -10,70 +11,11 @@ export function Home(){
             <div className="flex bg-stone-100 flex-col justify-center items-center">
                 <h1 className="mt-24 md:pb-10 font-semibold text-center text-2xl md:text-3xl font-playwrite">Please, be sure to call to check on current availability and pricing!</h1>
                     <ServiceCard />
-                    <h1 className="text-center md:text-4xl font-semibold text-2xl border-primary-green border-b-4 pb-2 font-playwrite">Our Promise</h1>
+                <h1 className="text-center md:text-4xl font-semibold text-2xl border-primary-green border-b-4 pb-2 font-playwrite">Our Promise</h1>
                 <div className="w-full md:px-10">
-                    <div className=" w-full px-4 flex items-center justify-start pb-10" >
-                        <div className="gap-10 flex flex-col bg-gray-100 shadow-md md:p-2 p-4 lg:flex-row max-w-6xl items-center lg:items-center">
-                            <img
-                            src='/assets/images/Sustainability.JPG'
-                            className=" w-[550px] h-[350px] object-cover object-top rounded"
-                            />
-                            <div>
-                                <div className="flex flex-col p-2 justify-center text-center items-center lg:items-center">
-                                <div className="px-2 mb-8">
-                                    <Sprout size={60} color="green"/>
-                                    </div>
-                                    <header className="md:text-2xl font-playwrite text-xl  font-semibold border-b-2 text-center mb-2">
-                                    Sustainability First
-                                    </header>
-                                    <p className=" pt-10 font-raleway text-center text-lg ">
-                                    We prioritize eco-friendly practices, ensuring that every tree and plant we grow supports a healthier environment.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" md:w-full px-4 flex items-end justify-end pb-10" >
-                        <div className="gap-10 flex bg-secondary-green shadow-md flex-col md:p-2 p-4 lg:flex-row-reverse max-w-6xl items-center lg:items-center">
-                            <img
-                            src='/assets/images/Hydrangea1.jpg'
-                            className=" w-[550px] h-[350px] object-cover object-top rounded"
-                            loading="eager"
-                            />
-                            <div>
-                                <div className="flex flex-col p-4 justify-center text-center items-center lg:items-en">
-                                <div className="px-2 mb-8">
-                                    <Leaf size={60} color="green"/>
-                                    </div>
-                                    <header className="md:text-2xl font-playwrite text-xl font-semibold border-b-2 text-center pb-1 mb-4">
-                                    Passion for Green
-                                    </header>
-                                    <p className=" pt-10 font-raleway text-center text-lg ">
-                                    We’re dedicated to our craft, bringing years of knowledge and passion to every product we offer.                                </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className=" md:w-full px-4 flex items-start justify-start pb-10" >
-                        <div className="gap-10 flex flex-col md:p-2 bg-gray-100 shadow-md p-4 lg:flex-row max-w-6xl items-center lg:items-center">
-                            <img
-                            src='/assets/images/Plants.jpg'
-                            className=" w-[550px] h-[350px] object-cover object-top rounded"
-                            />
-                            <div>
-                                <div className="flex flex-col p-4 justify-center text-center items-center lg:items-center">
-                                <div className="px-2 mb-8">
-                                    <Handshake size={60} color="green"/>
-                                    </div>
-                                    <header className="md:text-2xl text-xl font-playwrite font-semibold border-b-2 text-center pb-1 mb-4">
-                                    Honest Service
-                                    </header>
-                                    <p className=" pt-10 font-raleway text-center text-lg ">
-                                    You can trust us to provide the highest quality and most reliable service, with transparency and integrity at every step.                             </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <OurPromiseCards image={'/assets/images/Sustainability.JPG'} title={"Sustainability First"} icon={<Sprout size={60} color="green"/>} cardInfo={"We prioritize eco-friendly practices, ensuring that every tree and plant we grow supports a healthier environment."}  />
+                    <OurPromiseCards image={'/assets/images/Hydrangea1.jpg'} title={"Passion for Green"} icon={<Leaf size={60} color="green"/>} cardInfo={"We’re dedicated to our craft, bringing years of knowledge and passion to every product we offer."} style={true} />
+                    <OurPromiseCards image={'/assets/images/Plants.jpg'} title={"Honest Service"} icon={<Handshake size={60} color="green"/>} cardInfo={"You can trust us to provide the highest quality and most reliable service, with transparency and integrity at every step. "}  />
                 </div>
             </div>
         </>
