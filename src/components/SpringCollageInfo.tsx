5831fd6f80c4d87a467fcec8e82a5ceb12c9b930
Pete <pete.zink03@gmail.com>
2025-10-27 09:32:09 -0400

    Updated Homepage to containt 2nd annual chili cookoff promo

--- src/components/SpringCollageInfo.tsx
+++ src/components/SpringCollageInfo.tsx
@@ -7,32 +7,38 @@ export default function SpringCollageInfo() {
   return (
     <>
       <div className="w-full h-full flex flex-col justify-center items-center pt-20">
-        <header className="flex flex-col justify-center items-center text-center max-w-3xl">
-          <h1 className="flex text-2xl md:text-3xl capitalize font-playwrite mb-4 font-semibold border-b-2 border-secondary-darkgreen">
-            There's No Better Time to Plant!
-            <span className="pl-2">
-              <PiPlant color="green" size={25} />
-            </span>
+        <header className="flex flex-col justify-center items-center text-center ">
+          <h1 className="flex text-2xl md:text-3xl capitalize font-playwrite mb-4 max-w-2xl font-semibold border-b-2 border-secondary-darkgreen">
+            Join us for the 2nd Annual Chili Cook-Off at Nelson Lee Tree Farm!
           </h1>
           <p className="flex md:text-lg">
-            Planting season is upon us and Nelson Lee Tree Farm is fully stocked
-            with an incredible selection of trees, shrubs, and more. Stop by and
-            see what’s new, and be sure to follow us on our website and Facebook
-            for the latest updates and special offers!
+            Come enjoy a fall afternoon filled with delicious homemade chili,
+            live music, photo booths, and local vendors!
           </p>
-          <div className="py-4">
-            <Link to="/pricing" className="p-2">
-              <Button className="bg-secondary-darkgreen hover:scale-110 transition ease-in-out font-semibold text-base duration-300 hover:bg-secondary-green hover:text-secondary-darkgreen hover:border-secondary-darkgreen hover:border-2">
-                See Full Inventory
-              </Button>
-            </Link>
-            <Link to="/specialOffers" className="p-2">
-              <Button className="bg-secondary-darkgreen hover:scale-110 transition ease-in-out font-semibold text-base duration-300 hover:bg-secondary-green hover:text-secondary-darkgreen hover:border-secondary-darkgreen hover:border-2">
-                See Latest Special Offers
+          <div className="py-10 w-2/3">
+            <Link to="/specialEvents" className="p-2">
+              <Button className="w-full bg-secondary-darkgreen hover:scale-110 transition ease-in-out font-semibold text-base duration-300 hover:bg-secondary-green hover:text-secondary-darkgreen hover:border-secondary-darkgreen hover:border-2">
+                Sign Up Here!
               </Button>
             </Link>
           </div>
         </header>
+        <div className="flex max-w-4xl gap-x-10">
+          <img
+            src="/assets/images/2ndAnnualChiliFlyer.png"
+            className="md:h-[800px] md:w-[800px] h-full w-full "
+          />
+          {/* <div className="flex flex-col justify-center">
+            <h1 className="font-helvetica font-semibold text-2xl border-b-2 ">
+            Spring is Here
+            </h1>
+            <p className="flex justify-center font-raleway">
+            Spring is here. Come out and see us. We are always adding new
+            inventory so please call us if you're looking for anyhting in
+            particular.
+            </p>
+            </div> */}
+        </div>
         <ul className="flex flex-col md:flex-row justify-center items-start gap-2 text-lg py-2">
           {ICON_LINKS.map(({ id, child, style, text, href }) => (
             <li
@@ -56,22 +62,6 @@ export default function SpringCollageInfo() {
             </li>
           ))}
         </ul>
-        <div className="flex max-w-4xl gap-x-10 bg-secondary-green border-2 border-secondary-darkgreen rounded">
-          <img
-            src="/assets/images/SpringCollage.png"
-            className="md:h-[800px] md:w-[800px] h-full w-full "
-          />
-          {/* <div className="flex flex-col justify-center">
-            <h1 className="font-helvetica font-semibold text-2xl border-b-2 ">
-              Spring is Here
-            </h1>
-            <p className="flex justify-center font-raleway">
-              Spring is here. Come out and see us. We are always adding new
-              inventory so please call us if you're looking for anyhting in
-              particular.
-            </p>
-          </div> */}
-        </div>
       </div>
     </>
   )
